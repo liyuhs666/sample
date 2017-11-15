97588a88f6e41e77e922eb27bcfa375ec9e0b1ca
liyuhs666 <1301809569@qq.com>
2017-11-15 18:00:17 +0800

    test heroku

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', 'Sample App') - Laravel title</title>
+    <title>@yield('title', 'Sample App') - Test Heroku</title>
     <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
