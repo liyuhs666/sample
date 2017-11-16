2dacfc70a74991dc7feec39c0f83657f238a0b19
liyuhs666 <1301809569@qq.com>
2017-11-16 14:25:30 +0800

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
@@ -31,9 +32,13 @@ class UsersController extends Controller
     			'email' => $request->email,
     			'password' => bcrypt($request->password),
     		]);
+
+        Auth::login($user);
     	session()->flash('success',"It's a fine day with you around");
     	return redirect()->route('users.show',[$user]);
     }
+
+   
 
 
 }
