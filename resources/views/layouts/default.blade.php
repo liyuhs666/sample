696cedf2244cb2b34faff7a1af30c2fb847a6b9b
liyuhs666 <1301809569@qq.com>
2017-11-21 13:48:26 +0800

    change style

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', 'Sample App') - Test Heroku</title>
+    <title>@yield('title', 'MicroBlog') - Home</title>
     <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -21,9 +21,9 @@
     </div>
   @else
     <div class="jumbotron">
-      <h1>Hello Laravel</h1>
+      <h1>Playerunknown's Battlegrounds</h1>
       <p class="lead">
-        你现在所看到的是 <a href="https://laravel-china.org/laravel-tutorial/5.1">Playerunknown's Battlegrounds</a> 的主页。
+        你现在所看到的是 <a href="https://www.baidu.com">绝地求生</a> 的主页。
       </p>
       <p>
         一切，将从这里开始。
